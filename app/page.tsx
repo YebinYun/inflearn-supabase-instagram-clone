96d783f080088d1d18dc8cc67e377b45f88eb004
yebin <yebind3308@gmail.com>
2025-03-20 23:05:31 +0900

    Part 2 - 회원가입, 로그인 화면 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import LogoutButton from "components/auth/logout-button";
+
 export const metadata = {
   title: "Inflearngram",
   description: "Instagram clone project",
 };
 
 export default function Home() {
-  return <div className="">Inflearngram</div>;
+  return (
+    <main className="w-full h-screen flex flex-col gap-2 items-center justify-center">
+      <h1 className="font-bold text-xl">Welcome {"user"}!</h1>
+      <LogoutButton />
+    </main>
+  );
 }
---
+++ components/logout-button.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Button } from "@material-tailwind/react";
+
+export default function LogoutButton() {
+  return <Button color="red">·Î±×¾Æ¿ô</Button>;
+}
